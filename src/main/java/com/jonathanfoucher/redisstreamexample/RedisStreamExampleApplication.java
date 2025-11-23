8ee6a29b20bed5089f0419e3d146dc2a4fb79a34
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-11-23 14:11:50 +0100

    chore: upgrade dependencies and java versions

--- src/main/java/com/jonathanfoucher/redisstreamexample/RedisStreamExampleApplication.java
+++ src/main/java/com/jonathanfoucher/redisstreamexample/RedisStreamExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisStreamExampleApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(RedisStreamExampleApplication.class, args);
     }
 }
